d985fc67e5fe49c7ac027181ea923d99bb00b7a0
Sanders Gutiérrez <ing.sanders@gmail.com>
2022-10-13 01:24:50 -0300

    feat: Basic structure and update design.
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HomeComponent } from './pages/home/home.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { HeaderComponent } from './components/header/header.component'
 import { AboutComponent } from './pages/about/about.component'
 import { ContactComponent } from './pages/contact/contact.component'
-import { HeaderComponent } from './components/header/header.component'
-import { FooterComponent } from './components/footer/footer.component'
+import { HomeComponent } from './pages/home/home.component'
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ContactComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactComponent,
+    ],
     imports: [BrowserModule, AppRoutingModule],
     bootstrap: [AppComponent],
 })
